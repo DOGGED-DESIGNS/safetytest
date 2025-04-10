fbb868af80a1b453f47fb301e494e7c3af5c9089
dogged <uzoechijerry@gmail.com>
2025-04-10 13:09:35 +0100

    site

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+const siteUrl = "http://localhost:3000/";
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [{ userAgent: "*", allow: "/" }],
+  },
+};
---
+++ src/lib/utils.ts
@@ -0,0 +1,39 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+import { Metadata } from "next";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function constructMetadata({
+  title = "Sanagos Safety",
+  description = " At Sanagos Safety, we supply and service fire safety equipment,fire detection systems, and automatic fire suppression solutions—built to safeguard lives and property, 24/7.",
+  image = "/santhumb.png",
+  icons = "/favicon.ico",
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@sanagossafety",
+    },
+    icons,
+    metadataBase: new URL("https://sanagossafety.com/"),
+  };
+}
